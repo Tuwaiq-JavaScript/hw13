b1788f7e66738f6f026ada9433d1da81b0c15068
ranaqaboos <106505615+ranaqaboos@users.noreply.github.com>
2022-07-25 12:30:50 +0300

    rana 5

--- src/server.ts
+++ src/server.ts
@@ -30,14 +30,13 @@ server.get('/images', (req, reply) => {
 
 
 const port: any = process.env.PORT ?? process.env.$PORT ?? 3000;
-const host = process.env.HOST || "0.0.0.0";
 
 server
-  .listen({
-    port,
-    host,
-  })
-  .catch((err) => {
-    server.log.error(err);
-    process.exit(1);
-  });
+    .listen({
+        port: port,
+        host: '0.0.0.0',
+    })
+    .catch((err) => {
+        server.log.error(err);
+        process.exit(1);
+    });
