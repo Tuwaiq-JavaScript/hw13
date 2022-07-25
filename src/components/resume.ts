1c6276c748eccc6ce5f9dad5ad56d2c8dd9a9628
Essa <essa1417323@gmail.com>
2022-07-25 13:08:16 +0300

    gg

--- src/components/resume.ts
+++ src/components/resume.ts
@@ -15,7 +15,7 @@ export function Resumepage(content: string, color: string) {
          <meta name="viewport" content="width=device-width, initial-scale=1">
          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
            integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-           <link rel="stylesheet" href="http://localhost:4002/style.css">
+           <link rel="stylesheet" href="https://hw-thirteen.herokuapp.com//style.css">
            </head>
        
        <body>
