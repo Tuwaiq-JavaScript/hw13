014d829c1e9b872f9a9c567b75ecbcc955f7efd6
ranaqaboos <106505615+ranaqaboos@users.noreply.github.com>
2022-07-25 10:32:51 +0300

    rana

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import fastifyStatic from "@fastify/static";
+import Fastify from 'fastify';
+import { join } from 'path';
+import { Page } from './components/Page';
+
+const server = Fastify({ logger: true });
+
+server.register(fastifyStatic, {
+    root: join(__dirname, 'public'),
+    prefix: '/public/',
+})
+  
+
+server.get('/cv', (req, reply) => {
+    return reply.sendFile('public/cv.pdf')
+})
+
+server.get('/about', (req, reply) => {
+    reply.header('Content-Type', 'text/html; charset=utf-8')
+
+    return Page(' abuot me  mame:rana qaboos number:0500720216 birthday:15/7/1998','orange')
+})
+
+server.get('/style.css', (req, reply) => {
+    return reply.sendFile('style.css')
+})
+server.get('/images', (req, reply) => {
+    return reply.sendFile('images/ranaqaboos.jpeg')
+})
+
+
+server.listen({ port: 3000, host: '127.0.0.1' }, err => {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
+})
