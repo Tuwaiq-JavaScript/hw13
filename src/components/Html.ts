f9c10df347cf2a06f76c5ed23b452d518aab6ece
DaadAlSaif <daadalsaif@DaMss-MacBook-Pro.local>
2022-07-25 10:22:08 +0300

    first commit

---
+++ src/components/Html.ts
@@ -0,0 +1,16 @@
+export function Html(children: any){
+    
+return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script defer src="main.js"></script>
+    <link rel="stylesheet" href="http://0.0.0.0:4002/style.css">
+
+ ${children}
+</body>
+</html>`
+}
