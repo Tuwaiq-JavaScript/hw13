103265c3541a9a15f7101d1bb4abcfeb0be77c3d
Latefah18 <LatefahBarnawi@gmail.com>
2022-07-25 08:08:09 +0300

    Latefah hw13

---
+++ src/components/About.ts
@@ -0,0 +1,6 @@
+export function about(text: string, color: string) {
+    return `<div class="container">
+        <p class="${color}">${text}</p>
+        
+    </div>`
+}
---
+++ src/components/hobby.ts
@@ -0,0 +1,14 @@
+export function hobby(){
+    return `  <div class="flex">
+    <br>
+    <br>
+    <div class="view">
+    <h1>I love sports</h1>
+    <img src="public/images/download.png"running-man.jpg" alt="sports" class="sport">
+ </div>  
+     
+  <div class="view">
+   <img src="public/images/download1.jpeg" alt="the help book " class="Book">
+   <h1> I love books </h1>
+  </div>  `
+}
