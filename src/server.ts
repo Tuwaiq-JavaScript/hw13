ddb8b40ebdb843c9b008a6e8a0ee5dc2d308b92a
alWasayf <49132323+alWasayf@users.noreply.github.com>
2022-07-25 13:32:06 +0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -31,9 +31,14 @@ server.get('/cv', (req, reply) => {
 })
 
 
-server.listen({ port: 4002, host: '127.0.0.1' }, err => {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-})
+const port: any = process.env.PORT ?? process.env.$PORT ?? 4002;
+
+server
+	.listen({
+		port: port,
+		host: '0.0.0.0',
+	})
+	.catch((err) => {
+		server.log.error(err);
+		process.exit(1);
+	}); 
